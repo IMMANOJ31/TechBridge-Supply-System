f7dd55ef7edaca1d41608045f71cde4f958c9d4f
IMMANOJ31 <manojkumard.21is@saividya.ac.in>
2025-11-18 12:06:41 +0530

    init commit

--- src/main/java/com/techbridge/app/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/techbridge/app/service/impl/InvoiceServiceImpl.java
@@ -17,16 +17,25 @@ public class InvoiceServiceImpl implements InvoiceService {
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-        Document document = new Document();
-        PdfWriter.getInstance(document, outputStream);
+        try {
+            Document document = new Document();
+            PdfWriter.getInstance(document, outputStream);
 
-//        document.open();
-//        document.add(new Paragraph("INVOICE"));
-//        document.add(new Paragraph("Customer: " + purchase.getCustomerName()));
-//        document.add(new Paragraph("Item: " + purchase.getItemName()));
-//        document.add(new Paragraph("Quantity: " + purchase.getQuantity()));
-//        document.add(new Paragraph("Total Cost: " + purchase.getTotalCost()));
-//        document.close();
+            document.open();
+
+            document.add(new Paragraph("INVOICE"));
+            document.add(new Paragraph("------------------------------"));
+            document.add(new Paragraph("Customer: " + purchaseEntity.getCustomerName()));
+            document.add(new Paragraph("Product: " + purchaseEntity.getProductCode()));
+            document.add(new Paragraph("Quantity: " + purchaseEntity.getQuantity()));
+            document.add(new Paragraph("Total Cost: " + purchaseEntity.getTotalCost()));
+            document.add(new Paragraph("------------------------------"));
+
+            document.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return outputStream.toByteArray();
     }
